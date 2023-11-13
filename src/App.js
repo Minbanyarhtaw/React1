8b686e95129e3a31263355277ef6c712ddbb77f0
Banyar <webdevelopingbanyar@gmail.com>
2023-11-13 08:34:13 +0630

    button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from "react";
-import AddUser from "./component/User/Adduser";
-// import Card from "../UI/Card";
-const App =()=>{
-  return(
-    <div>
-      <AddUser/>
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
+
 export default App;
